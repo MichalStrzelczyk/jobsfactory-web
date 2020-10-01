4129ece5439811f2acef58c935b76a5413c403d7
Arrow <michal.strzelczyk@miinto.com>
2020-10-01 09:45:29 +0200

    Add Image component

---
+++ src/components/structural/Image.vue
@@ -0,0 +1,43 @@
+<template>
+  <img src="" alt="" />
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data: function () {
+    return {
+
+    }
+  },
+  methods: {
+    mounted() {
+      this.getImage(this.image, this.companyName);
+    },
+    getImage: function(imageUrl, companyName) {
+      axios
+          .get(imageUrl)
+          .then(() => {
+            this.data.imageUrl = imageUrl;
+            this.data.title = companyName;
+          })
+          .catch(error => console.log(error))
+    }
+  },
+  name: 'Image',
+  props: {
+    image: {
+      type: String
+    },
+    companyName: {
+      type: String
+    }
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
